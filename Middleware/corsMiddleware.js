2f8bf29c895d8f7d76316f37a208ac693efdd4c8
Jenny Thais <jennythai.fw@gmail.com>
2024-10-30 14:51:12 +0700

    config

--- Middleware/corsMiddleware.js
+++ Middleware/corsMiddleware.js
@@ -1,11 +1,14 @@
 function corsMiddleware(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // Trả về trạng thái OK cho preflight request
+  }
   next();
 }
 
